3a1c472ba045784f3130bd052827cb47bfe5c3d6
Paschaline <apasscar1@gmail.com>
2020-05-09 23:56:37 +0100

    Json

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,6 +12,7 @@ class _QuizPageState extends State<QuizPage> {
   var questions = [];
   int index = 0;
 
+
   int rightAnswerCount = 0;
   int wrongAnswerCount = 0;
   Color right= Colors.green;
@@ -21,27 +22,31 @@ class _QuizPageState extends State<QuizPage> {
   void initState() {
   
     super.initState();
-    this.fetchQuestion();
+    this.setAsset();
   }
 
-  fetchQuestion() {
-    http.get("https://opentdb.com/api.php?amount=10").then((res) {
-      var resp = jsonDecode(res.body);
-      setState(() {
+  setAsset() {
+     String assettoload = "assets/general_question.json";
+    http.get(assettoload).then((res) {
+    var resp = jsonDecode(res.body);
+    setState(() {
         questions = resp["questions"];
-      });
-    }).catchError((err) {
-      print(err);
     });
-  }
-
+  }).catchError((err) {
+  print(err);
+  });
+}
   validate(i) {
-    if (questions[index]["answerIndex"] == i) {
-      setState(() { 
+    if (questions[index]["correctIndex"] == i) {
+
+      setState(() {
+
         rightAnswerCount++;
       });
     } else {
+
       setState(() {
+
         wrongAnswerCount++;
       });
     }
@@ -79,17 +84,21 @@ class _QuizPageState extends State<QuizPage> {
             RaisedButton(
               onPressed: () => validate(0),
               child: Text(questions[index]["options"][0]),
+              color: Colors.blue,
             ),
             RaisedButton(
               onPressed: () => validate(1),
               child: Text(questions[index]["options"][1]),
+              color: Colors.blue,
             ),
             RaisedButton(
               onPressed: () => validate(2),
+              color: Colors.blue,
               child: Text(questions[index]["options"][2]),
             ),
             RaisedButton(
               onPressed: () => validate(3),
+              color: Colors.blue,
               child: Text(questions[index]["options"][3]),
             ),
           ],
--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class ResultPage extends StatelessWidget {
   var data;
 
